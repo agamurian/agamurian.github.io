127b923b87a03fb18595593ea02eea0e1162c3d0
Andrey Golovin <agamurian@outlook.com>
2023-05-17 20:50:25 +0200

    rebuilded w sveltekit

---
+++ _app/immutable/nodes/3.6808e128.js
@@ -0,0 +1,20 @@
+import{S as ga,i as Ea,s as ka,k as t,q as u,a as c,l as r,m as s,r as m,h as l,c as h,n as p,b as Aa,G as a,u as va,J as ba,O as ya}from"../chunks/index.32f5b285.js";import{b as Pa,l as Ra}from"../chunks/stores.5db7a0c8.js";function wa(f){let e,n,d,_=f[1].name[f[0]]+"",v,S,b,D=f[1].text[f[0]]+"",G,$,q,K,g,E,x,H,J,k,A,L,N,V,y,P,j,z,F,R,w,Q,U,W,B,M,X,Y,Z,T,C,aa,ea,ta,I;return{c(){e=t("article"),n=t("div"),d=t("h1"),v=u(_),S=c(),b=t("p"),G=u(D),$=c(),q=t("br"),K=c(),g=t("p"),E=t("a"),x=u("agamurian@outlook.com"),H=t("br"),J=c(),k=t("p"),A=t("a"),L=u("ag-3d@ya.ru"),N=t("br"),V=c(),y=t("p"),P=t("a"),j=u("artstation"),z=t("br"),F=c(),R=t("p"),w=t("a"),Q=u("linkedin"),U=t("br"),W=c(),B=t("p"),M=t("a"),X=u("github"),Y=t("br"),Z=c(),T=t("p"),C=t("a"),aa=u("gitlab"),ea=t("br"),ta=c(),I=t("p"),this.h()},l(i){e=r(i,"ARTICLE",{class:!0});var o=s(e);n=r(o,"DIV",{class:!0});var O=s(n);d=r(O,"H1",{});var ca=s(d);v=m(ca,_),ca.forEach(l),S=h(O),b=r(O,"P",{class:!0});var ha=s(b);G=m(ha,D),ha.forEach(l),O.forEach(l),$=h(o),q=r(o,"BR",{}),K=h(o),g=r(o,"P",{});var ra=s(g);E=r(ra,"A",{mailto:!0});var pa=s(E);x=m(pa,"agamurian@outlook.com"),pa.forEach(l),H=r(ra,"BR",{}),ra.forEach(l),J=h(o),k=r(o,"P",{});var oa=s(k);A=r(oa,"A",{mailto:!0});var da=s(A);L=m(da,"ag-3d@ya.ru"),da.forEach(l),N=r(oa,"BR",{}),oa.forEach(l),V=h(o),y=r(o,"P",{});var la=s(y);P=r(la,"A",{href:!0});var ua=s(P);j=m(ua,"artstation"),ua.forEach(l),z=r(la,"BR",{}),la.forEach(l),F=h(o),R=r(o,"P",{});var sa=s(R);w=r(sa,"A",{href:!0});var ma=s(w);Q=m(ma,"linkedin"),ma.forEach(l),U=r(sa,"BR",{}),sa.forEach(l),W=h(o),B=r(o,"P",{});var na=s(B);M=r(na,"A",{href:!0});var fa=s(M);X=m(fa,"github"),fa.forEach(l),Y=r(na,"BR",{}),na.forEach(l),Z=h(o),T=r(o,"P",{});var ia=s(T);C=r(ia,"A",{href:!0});var _a=s(C);aa=m(_a,"gitlab"),_a.forEach(l),ea=r(ia,"BR",{}),ia.forEach(l),ta=h(o),I=r(o,"P",{}),s(I).forEach(l),o.forEach(l),this.h()},h(){p(b,"class","whitespace-pre-line"),p(n,"class","lg:w-3/4 md:w-2/3 sm:w-full"),p(E,"mailto","agamurain@outlook.com"),p(A,"mailto","ag-3d@ya.ru"),p(P,"href","https://agamura.artstation.com"),p(w,"href","https://linkedin.com/in/agamura"),p(M,"href","https://github.com/agamurian"),p(C,"href","https://gitlab.com/agamura"),p(e,"class","p-5")},m(i,o){Aa(i,e,o),a(e,n),a(n,d),a(d,v),a(n,S),a(n,b),a(b,G),a(e,$),a(e,q),a(e,K),a(e,g),a(g,E),a(E,x),a(g,H),a(e,J),a(e,k),a(k,A),a(A,L),a(k,N),a(e,V),a(e,y),a(y,P),a(P,j),a(y,z),a(e,F),a(e,R),a(R,w),a(w,Q),a(R,U),a(e,W),a(e,B),a(B,M),a(M,X),a(B,Y),a(e,Z),a(e,T),a(T,C),a(C,aa),a(T,ea),a(e,ta),a(e,I)},p(i,[o]){o&1&&_!==(_=i[1].name[i[0]]+"")&&va(v,_),o&1&&D!==(D=i[1].text[i[0]]+"")&&va(G,D)},i:ba,o:ba,d(i){i&&l(e)}}}function Ba(f,e,n){let d;return ya(f,Ra,v=>n(0,d=v)),Pa.set("#111111"),[d,{name:{en:"Andrey Golovin",ru:"Андрей Головин"},text:{en:`
+				Designer, Dveloper, and Media Artist.
+				Mainly work as Technical Artsit in game development or as freelance web developer.
+				
+				Opened contemprorary art Gallery Kerka.
+
+				Education:
+				ITMO university : Teacher of computer graphics
+				Rodchenko Art School: New Media
+			`,ru:`
+				Дизайнер, Разработчик и Медиа-художник.
+				В основном работаю как Технический художник в разработке игр или как независивый веб разработчик.
+
+				Открыл галерею современного искусства Kerka
+
+				Образование:
+				ИТМО: преподаватель компьютерной графики
+				СПБГМТУ: художник-технолог по обработке материалов
+				Школа Родченко: Новые Медиа 
+			`}}]}class Ca extends ga{constructor(e){super(),Ea(this,e,Ba,wa,ka,{})}}export{Ca as component};
