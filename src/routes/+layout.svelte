1011b10e988bc5f825e2b001ebae6c2180295e0f
Andrey Golovin <agamurian@outlook.com>
2023-05-18 23:57:38 +0200

    github action test again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,23 @@
 	import Navbar from '$lib/components/Navbar/Navbar.svelte';
 	import BackgroundCanvas from '$lib/components/Canvas/BackgroundCanvas.svelte';
 	import { page } from '$app/stores';
+	$: title = $page.url.pathname.split('/').join(' | ') + ' | Agamurian';
+	const description = 'Designer, Dveloper, Media-Artist';
 </script>
 
 <svelte:head>
 	<!--<link rel="stylesheet" href="/tutorial/dark-theme.css">-->
-	<title>{$page.url.pathname.split('/').join(' | ') + ' | Agamurian'}</title>
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta name="description" content={description} />
+	<meta name="twitter:description" content={description} />
+	<meta property="og:description" content={description} />
+
+	<meta property="og:image" content="" />
+	<meta property="og:image:width" content="200" />
+	<meta property="og:image:height" content="200" />
+	<meta name="twitter:image" content="" />
+	<meta itemprop="image" content="" />
 </svelte:head>
 
 <Navbar />
