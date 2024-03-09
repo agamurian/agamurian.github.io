e24e3414f1d69c129a50de6ecbbb6f42b3c6c36b
Andrey Golovin <agamurian@gmail.com>
2024-03-09 16:36:37 +0100

    adding gallery separators

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -30,30 +30,42 @@
 </script>
 
 <article class="p-5">
-	<img class='img' src="/gallery/1.webp"/>
-	<div class="text">image one</div>
+	<img  class='img' src="/gallery/nine.webp"/>
+	<div class="text">image nine</div>
+	<div class="separator">{'- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'}</div>
 	<img  class='img' src="/gallery/3.webp"/>
 	<div class="text">image eight</div>
+	<div class="separator">{'- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'}</div>
 	<img  class='img' src="/gallery/4.webp"/>
 	<div class="text">image nine</div>
-	<img class='img' src="/gallery/5.webp"/>
-	<div class="text">image numbre one</div>
 	<img class='img' src="/gallery/6.webp"/>
 	<div class="text">image numbre one</div>
+	<img class='img' src="/gallery/5.webp"/>
+	<div class="text">image numbre one</div>
+	<div class="separator">{'- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'}</div>
 	<img class='img' src="/gallery/w1.webp"/>
 	<div class="text">image one</div>
 	<img  class='img' src="/gallery/w7.webp"/>
 	<div class="text">image seven</div>
-	<img  class='img' src="/gallery/2.webp"/>
-	<div class="text">Coffee and Orange</div>
 	<img  class='img' src="/gallery/w8.webp"/>
 	<div class="text">image eight</div>
 	<img  class='img' src="/gallery/w9.webp"/>
 	<div class="text">image nine</div>
+	<img  class='img' src="/gallery/2.webp"/>
+	<div class="text">Coffee and Orange</div>
 </article>
 
 <style lang="css">
 
+	.separator {
+		color: antiquewhite;
+		opacity: 0.1;
+		font-family: 'Golos Text';
+		font-size: 3.0vw;
+		margin: 0.5rem;
+		margin-bottom: 4.0rem;
+		text-align: center;
+	}
 	.text {
 		color: antiquewhite;
 		opacity: 0.6;
@@ -61,7 +73,7 @@
 		font-weight: 600;
 		font-size: 0.6rem;
 		margin: 1rem;
-		margin-bottom: 5rem;
+		margin-bottom: 4rem;
 		text-align: center;
 	}
 	.img {
