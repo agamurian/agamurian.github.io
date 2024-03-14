b001e4ec3889978d961953bfb3b5a7530c589847
Andrey Golovin <agamurian@gmail.com>
2024-03-14 22:36:42 +0100

    initial bck value

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,7 +3,7 @@ import {writable} from "svelte/store";
 
 export const hideNav = writable<boolean>(false)
 export const theme = writable("light");
-export const backgroundColor = writable("#454545");
+export const backgroundColor = writable("#BA3121");
 
 type language = "en" | "ru"
 export const lang = writable<language>("en")
